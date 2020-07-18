b110f50b01d13cd61e0812b9355db03237518abc
Ezequiel Zarate <cezarate@live.com.ar>
2020-07-18 09:16:11 -0300

    Version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { VersionService } from './services/version.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'word-finder';
-  constructor() {
+  constructor(private versionService: VersionService) {
+    this.versionService.set('{{ci-version}}');
   }
 }
